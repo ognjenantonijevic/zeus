71391a3daa073c5db5dee57d5c52e679b6c79cf3
Ognjen Antonijevic <antonijevic.ognjen@gmail.com>
2024-12-13 11:41:33 +0100

    Changes, introduced drop orders and truck book dates and sanity checks, currently 72% percent booked, and some 5000 tires missing with sanity checks (Maybe split orders bug or?) IN the next commits will swith to day by day booking instead of traversing only MAD days (when tires arrive), since there were wh capacity errors

--- zeus.py
+++ zeus.py
@@ -13,10 +13,10 @@ orders_f = f'{basedir}/Order Wallet + Shipping Vol Wt 20240912.xlsx'
 wh_f = f'{basedir}/Apollo Warehouse Address V2 20240729_GILAB.xlsx'
 vh_f = f'{basedir}/Zeus - Apollo - list of trailer types 20240902.xlsx'
 vh = pd.read_excel(vh_f)
-vh_type_mapping = {'STANDARD_TRAILER':'tautliner-13-6m-44ft',
-                   'XL_TRAILER':'high-cube-or-tall-triler',
-                    'BOX_TRAILER':'box-13-6m-44ft', 
-                    'MEGA_TRAILER':'mega-trailer-13-6m-44ft'}
+vh_type_mapping = {'STANDARD_TRAILER':'13.6m/45ft Curtainsider/ Tautliner',
+                   'XL_TRAILER':'High Cube/Tall Trailer',
+                    'BOX_TRAILER':'13.6m/45ft Box', 
+                    'MEGA_TRAILER':'13.6m/45ft 13.6m/45ft Mega trailer'}
 vh['vehicleType'] = vh['Apollo (Equip type ID)'].map(vh_type_mapping)
 # use only some trailers?
 orders = pd.read_excel(orders_f)
@@ -30,11 +30,14 @@ cust = pd.read_csv(cust_f, header=None)
 cust.columns = ['Ship to Party','party_name','party_address','party_city','party_zipcode','party_country']
 cust['Ship to Party'] = cust['Ship to Party'].astype(str)
 cust.set_index('Ship to Party',inplace=True)
-contracts = pd.read_csv(f'{basedir}/contracts.csv')
-contracts = contracts[contracts['price'] > 0]
+contracts = pd.read_excel(f'{basedir}/Updated Rate Card V2 dt 20240902.xlsx')
+#/contracts.csv')
+contracts = contracts[contracts['Price'] > 0]
 hauls = contracts.copy(deep=True)
+hauls['regionCodeTo'] = hauls['Ship To Country'].str.upper() + '-' + hauls['Ship To Postal Code Prefix (2 digits)'].astype(str)
 hauls['available'] = True
 hauls['availableFrom'] = None
+hauls.rename(columns={'Vehicle Type':'vehicleType'},inplace=True)
 # Add vehicle type from apollo
 hauls = pd.merge(hauls,vh,on='vehicleType')
 whs = pd.read_excel(wh_f)
@@ -46,6 +49,8 @@ abs_min_qty = 400 # Drop customers below certain treshold, TEMPORARY APPROX
 additionalKmRate = 1.15
 freeKmAmount = 75
 fuelSurchargePercent = 6.14
+urg_thr = pd.Timedelta(days=7)# Urgent threshold, if less than 7 days left
+drp_thr = pd.Timedelta(days=2) # If less than 2 days left, drop order
 
 def wh_pack_orders(wh_id, orders):
     # Work per warehouse, maybe later see if we can combine orders from multiple warehouses into one truck (if they are close enough)
@@ -56,7 +61,7 @@ def wh_pack_orders(wh_id, orders):
     # wh_ord['Balance Order Quantity'].sum()
     wh_city = wh['City'].values[0]
     wh_dep_t = 12 # departure time, HARD CODED FOR NOW
-    wh_haul = hauls[(hauls['cityFrom'] == wh_city)] # TODO: Get only hauliers that are currently available, keep track of departure and return dates
+    wh_haul = hauls[(hauls['Shipping Point'] == wh_id)] 
     # Get order availability dates
     dates = wh_ord['MAD'].dt.date.unique()
     dates.sort()
@@ -66,11 +71,15 @@ def wh_pack_orders(wh_id, orders):
     b_truck = [] # Booked trucks
     ord_backlog = []
     cuopt_ords = []
+    dropped = []
     # Traverse dates
+    d_i = -1
     for d in dates:
+        d_i+=1
         print(d)
+        tday = pd.Timestamp(d)
         # See if any of the hauliers returned
-        wh_haul['available'] = wh_haul['availableFrom'] <= d # TODO: Check
+        wh_haul.loc[:,'available'] = wh_haul['availableFrom'] <= d # TODO: Check
         wh_haul_a = wh_haul[wh_haul['available']]
         wh_cap = wh_capacities[wh_id] # Warehouse capacity, each day new
         #d = dates[0]
@@ -80,9 +89,13 @@ def wh_pack_orders(wh_id, orders):
         d_ord.loc[:,'Total Volume'] = d_ord['Balance Order Quantity']*d_ord['Volume per tyre']*volume_scale
         # Doda se odmah i lokacija, posle se iz d_ord samo uzimaju podaci, na osnovu razlicitih grupisanja
         d_ord = add_ord_loc(d_ord,cust)
-        d_ord['loc'] = d_ord['party_country']+'-'+d_ord['party_zipcode'].str[:2]
+        d_ord.loc[:,'loc'] = d_ord['party_country']+'-'+d_ord['party_zipcode'].str[:2]
         # Merge backlog with new orders
         d_ord = pd.concat([d_ord] + ord_backlog)
+        # Check which ords from backlog need to be dropped NOW
+        drop = d_ord[ tday >= d_ord['DD'] -  drp_thr] # drop orders <2days left
+        dropped.append(drop)
+        d_ord = d_ord[ tday < d_ord['DD'] -  drp_thr] # keep orders >2days left
         ord_backlog = []
         #--------# Group by customer
         d_ord_sum = d_ord.groupby(['Ship to Party']).agg({'Gross total weight':sum,'Total Volume':sum,'Balance Order Quantity':sum}) # ,'SKU Code']
@@ -104,32 +117,29 @@ def wh_pack_orders(wh_id, orders):
                 if wh_cap==0:
                     # If no more wh capacity
                     print('No more wh capacity')
-                    not_more_capacity = True #TODO: Check
                     orders_left.append(d_ord_c)
                     continue
                 # Pull wh_hauliers for this customer and start filling
                 haul_loc=d_ord_c.iloc[0]['loc'].upper()
                 c_haul = wh_haul_a[wh_haul_a['regionCodeTo']==haul_loc]
-                print(haul_loc)
-                print(c_haul)
                 if c_haul.empty:
                     # If no hauliers for this customer, add to backlog
                     print('No hauliers for this customer')
                     orders_left.append(d_ord_c)
                     continue
-                c_haul = c_haul.sort_values('price')
+                c_haul = c_haul.sort_values('Price')
                 # Book orders, and return booked orders and trucks, and left orders and trucks, split orders and warehouse capacity
-                d_ord_t, d_ord_b, d_ord_rem_o, wh_cap = fill_trucks_customer(d_ord_c, c_haul, wh_cap)
+                d_ord_t, d_ord_b, d_ord_rem_o, wh_cap = fill_trucks_customer(d_ord_c, c_haul, wh_cap, d)
                 b_ord+=d_ord_b
                 b_truck+=d_ord_t
                 # Remove booked hauliers from available hauliers
                 booked_h = [t['truck'] for t in d_ord_t]
-                wh_haul_a = wh_haul_a[~wh_haul_a['contractNumber'].isin(booked_h)]
-                wh_haul.loc[wh_haul['contractNumber'].isin(booked_h),'available'] = False
-                wh_haul.loc[wh_haul['contractNumber'].isin(booked_h),'availableFrom'] = d + pd.Timedelta(days=5)
+                wh_haul_a = wh_haul_a[~wh_haul_a['Haulier ID'].isin(booked_h)]
+                wh_haul.loc[wh_haul['Haulier ID'].isin(booked_h),'available'] = False
+                wh_haul.loc[wh_haul['Haulier ID'].isin(booked_h),'availableFrom'] = d + pd.Timedelta(days=5)
                 # TODO: Add return date for each haulier
                 # If there are unbooked orders, add them to backlog
-                if d_ord_rem_o:
+                if not d_ord_rem_o.empty:
                     orders_left.append(d_ord_rem_o)
         #--------# Merge xl ord leftovers and small orders from c grouping
         orders_left = orders_left + [ord_sml]
@@ -167,18 +177,18 @@ def wh_pack_orders(wh_id, orders):
                     # If no hauliers for this location, add to backlog
                     orders_left.append(d_ord_left_xl_l)
                     continue
-                l_haul = l_haul.sort_values('price')
+                l_haul = l_haul.sort_values('Price')
                 # Book orders, and return booked orders and trucks, and left orders and trucks, split orders and warehouse capacity
-                d_ord_t, d_ord_b, d_ord_rem_o, wh_cap = fill_trucks_customer(d_ord_left_xl_l, l_haul, wh_cap)
+                d_ord_t, d_ord_b, d_ord_rem_o, wh_cap = fill_trucks_customer(d_ord_left_xl_l, l_haul, wh_cap, d)
                 b_ord+=d_ord_b
                 b_truck+=d_ord_t
                 # Remove booked hauliers from available hauliers
                 booked_h = [t['truck'] for t in d_ord_t]
-                wh_haul_a = wh_haul_a[~wh_haul_a['contractNumber'].isin(booked_h)]
-                wh_haul.loc[wh_haul['contractNumber'].isin(booked_h),'available'] = False
-                wh_haul.loc[wh_haul['contractNumber'].isin(booked_h),'availableFrom'] = d + pd.Timedelta(days=5)
+                wh_haul_a = wh_haul_a[~wh_haul_a['Haulier ID'].isin(booked_h)]
+                wh_haul.loc[wh_haul['Haulier ID'].isin(booked_h),'available'] = False
+                wh_haul.loc[wh_haul['Haulier ID'].isin(booked_h),'availableFrom'] = d + pd.Timedelta(days=5)
                 # If there are split orders and unbooked orders, add them to backlog
-                if d_ord_rem_o:
+                if not d_ord_rem_o.empty:
                     orders_left.append(d_ord_rem_o)
         #------# Merge xl ord leftovers and small orders from loc grouping
         orders_left = orders_left + [d_ord_left_small]
@@ -188,13 +198,12 @@ def wh_pack_orders(wh_id, orders):
             ord_backlog = orders_left
             continue
         #--------# Country grouping
+        # After all is booked for the day, if wh_cap>0, agg by country(cuOpt)
         ord_cntry = pd.concat(orders_left)
         ord_cntry.reset_index(inplace=True, drop=True)
         # Group by country and city to try and fill truck for city
         ord_cntry_sum = ord_cntry.groupby(['party_country']).agg({'Gross total weight':sum,'Total Volume':sum,'Balance Order Quantity':sum})
         ord_cntry_sum.sort_values('Total Volume',ascending=False, inplace=True)
-        #################### OGI##########
-        ########## OVDE STAVI ISTO KAO GORE, ONO STO OSTANE IDE PO COUTRY
         # Continue with the same logic as above, for full trucks or >900
         ord_cntry_sum_xl= ord_cntry_sum[ord_cntry_sum['Balance Order Quantity']>=min_qty]
         ord_cntry_sum_sml= ord_cntry_sum[ord_cntry_sum['Balance Order Quantity']<min_qty]
@@ -203,31 +212,75 @@ def wh_pack_orders(wh_id, orders):
         if not ord_cntry_sum_xl.empty:
             print('Big orders for one or more countries')
             for cnt in ord_cntry_sum_xl.index:
-                ord_cntry_cnt = ord_cntry[ord_cntry['party_country']==cnt]
+                ord_cntry_cnt = ord_cntry.loc[ord_cntry['party_country']==cnt]
                 if wh_cap==0:
                     # If no more wh capacity
                     ord_backlog.append(ord_cntry_cnt)
                     continue
                 # Pull wh_hauliers for this country
-                cnt_haul = wh_haul_a[wh_haul_a['countryTo']==cnt.upper()]
+                cnt_haul = wh_haul_a.loc[wh_haul_a['Ship To Country']==cnt]
                 if cnt_haul.empty:
                     print('No hauliers for this country')
                     # If no hauliers for this location, add to backlog
                     ord_backlog.append(ord_cntry_cnt)
                     continue
-                cnt_haul = cnt_haul.sort_values('price')
+                cnt_haul = cnt_haul.sort_values('Price')
                 ord_cuopt = ord_cntry_cnt
                 cuopt_ords.append(ord_cuopt)
-        # OVDE SAD IDE PROVERA ZA ORDERE KOJI SU PRED ISTEK
+        # Check which orders from backlog are near delivery date, these have to be booked
+        backlog = pd.concat(ord_backlog)
+        non_urgent = backlog[tday < backlog['DD'] -  urg_thr]
+        # Check if there are orders that are near delivery date (7d or less)
+        urgent = backlog[tday >= backlog['DD'] -  urg_thr]
+        # if d_i+1>=len(dates):
+        #     # If this is the last day, all orders have to be booked
+        #     ord_backlog = [non_urgent,urgent]
+        #     continue
+        # else:
+        #     nxt_mad_d = pd.Timestamp(dates[d_i+1])
+        #     # Check which ords will be late till next MAD
+        #     late = urgent[nxt_mad_d < urgent['DD'] -  drp_thr]
+        ord_backlog = [non_urgent,urgent]
+        
+
+
+
 
         
-        # After all that is possible is booked for this day, if there is still wh capacity, agg by country and try cuOpt
-        # Sanity check
-        wh_ord['Balance Order Quantity'].sum()
-        b_ord  # Booked orders
+        ######### Sanity check ##############
+        a = wh_ord['Balance Order Quantity'].sum()
+        #b_ord  # Booked orders
+        b = sum([ b['quantity'] for b in b_ord])
+        # Check quantity in trucks
+        t_qnty = sum([sum(b_o['quantity'] for b_o in t['orders']) for t in b_truck])
+        b == t_qnty
+        # Percent booked
+        b_perc = b/a
+        print(f'Booked {b_perc*100:.2f}% of orders')
+        # Check if all booked orders have delivery date at least 2 days from truck book date
+        late_booked = []
+        for t in b_truck:
+            for o in t['orders']:
+                o_id = o['order']
+                o_dd = orders.loc[o_id]['DD']
+                t_dd = pd.Timestamp(t['date'])
+                if o_dd < t_dd + drp_thr:
+                    print('Order booked too late')
+                    late_booked.append(o_id)
+        len(late_booked)
+        #
+        ords_dropped = pd.concat(dropped)
+        d = ords_dropped['Balance Order Quantity'].sum()
+        ords_cuopt = pd.concat(cuopt_ords)
+        cpt = ords_cuopt['Balance Order Quantity'].sum()
+        ord_left = pd.concat(ord_backlog)
+        l = ord_left['Balance Order Quantity'].sum()
+        print(a)
+        print(b + d + cpt + l)
+        a == b + d + cpt + l
+        
         b_truck
         ord_backlog
-        ord_cuopt['Balance Order Quantity'].sum()
         ###############################################
         # for o in ord_backlog:
         #         if wh_cap==0:
--- zeus_utils.py
+++ zeus_utils.py
@@ -1,5 +1,8 @@
+import pandas as pd
 
 
+
+volume_scale = 0.8
 
 def add_ord_loc(orders_sum,customers):
     ord = orders_sum
@@ -7,65 +10,105 @@ def add_ord_loc(orders_sum,customers):
     ord = ord.join(cust, on=["Ship to Party"], how="left")
     return ord
 
-def fill_trucks_customer(orders, trucks, wh_cap=25):
+# TODO: This is temporary, in the future a better function for truck filling is needed, which calculates more precicely volume decrease when using tire lacing (cris-cross packing), and which takes into account that bigger tires can't be packed like the smaller ones (no volume decrease). In short - a special tire packing algorithm is needed (container filling problem variant)
+
+def fill_trucks_customer(orders, trucks, wh_cap=25, date=None):
     # These are all orders for one customer or location. Truck should be filled one by one until all are packed or until there are no more trucks or until warehouse capacity is reached. Return orders that are not packed, trucks that are not filled and booked orders.
     # Sort orders by volume
     orders = orders.sort_values('Total Volume',ascending=False)
+    min_v = orders['Volume per tyre'].min() + 0.001 # minimum volume needed for one tire
     # Start filling from cheapest truck
-    trucks = trucks.sort_values('price')
+    trucks = trucks.sort_values('Price')
     cur_o = 0
     # Verovatno treba imati i podatak o narudzbinama u kamionu, isto tako i u tome kojim sve kamionima ide neka veca narudzbina, i da li je cela poslata itd.
-    booked_t = []#{'truck':h_id, 'status':'part-full','orders':[{o_id:o_vol}]}]
-    booked_o = []#{'order':o_id, 'truck':h_id, 'volume':o_vol}]
+    booked_t = []#{'truck':h_id, 'status':'part-full','orders':[{'order':o_id, 'quantity':o_qty, 'volume':o_vol}]},'remaining_v':0}]
+    booked_o = []#{'order':o_id, 'truck':h_id, 'volume':o_vol, 'quantity':o_qty}]
     split_o = None #[{'order':o_id, 'remaining_v':o_vol}]
     for i,r in trucks.iterrows():
         if len(booked_t)>=wh_cap:
             break
-        t_vol = r['Effective Volume'] # capacity
-        h_id = r['contractNumber'] # haulier
+        t_vol = r['Effective Volume'] # truck capacity
+        h_id = r['Haulier ID'] # haulier
         t = {'truck':h_id, 'status':'empty','orders':[]}
+        added_os = [o.key() for o in t['orders']]
         vol = 0
+        # New truck, check if there is a split order
         if split_o:
-            vol=split_o['remaining_v']
-            if vol==t_vol:
+            sp_o_id = split_o['order']
+            sp_t_v = split_o['tire_v']
+            sp_vol=split_o['remaining_v']
+            sp_qty=split_o['remaining_q']
+            if sp_vol==t_vol:
                 # the remaining volume is exactly the same as the truck volume
+                if sp_o_id not in added_os:
+                    t['orders'].append({'order':sp_o_id, 'quantity':sp_qty, 'volume':sp_vol})
                 t['status']='full'
+                t['remaining_v']=0
+                if date:
+                    t['date'] = date
                 booked_t.append(t)
                 wh_cap-=1
-                booked_o.append({'order':split_o['order'], 'truck':h_id,
-                                 'volume':vol})
-                cur_o+=1
+                if sp_o_id not in [o['order'] for o in booked_o]:
+                    booked_o.append({'order':sp_o_id, 'truck':h_id,
+                                'volume':vol, 'quantity':sp_qty})
+                else:
+                    for b_o in booked_o:
+                        if b_o['order']==sp_o_id:
+                            b_o['volume']+=sp_vol
+                            b_o['quantity']+=sp_qty
+                cur_o+=1 # move to the next order
                 split_o = None
                 continue
-            elif vol>t_vol:
+            elif sp_vol>t_vol:
                 # the remaining volume is larger than the truck volume
-                t['orders'].append({split_o['order']:t_vol})
-                t['status']='full'
+                # This truck will be filled with part of this order
+                # How many tires can be added to the truck
+                t_fit_qty = t_vol//(sp_t_v * volume_scale)
+                t_fit_vol = t_fit_qty*sp_t_v * volume_scale
+                t_rem_vol = t_vol-t_fit_vol
+                if sp_o_id not in [o['order'] for o in booked_o]:
+                    booked_o.append({'order':sp_o_id, 'truck':h_id,
+                        'volume':t_fit_vol, 'quantity':t_fit_qty})
+                else:
+                    for b_o in booked_o:
+                        if b_o['order']==sp_o_id:
+                            b_o['volume']+=t_fit_vol
+                            b_o['quantity']+=t_fit_qty
+                t['orders'].append({'order':sp_o_id, 'quantity':t_fit_qty,  'volume':t_fit_vol})
+                if t_rem_vol>min_v:
+                    t['status']='part-full'
+                    t['remaining_v']=t_rem_vol
+                if date:
+                    t['date'] = date
                 booked_t.append(t)
                 wh_cap-=1
-                booked_o.append({'order':split_o['order'], 'truck':h_id,
-                                 'volume':t_vol})
-                split_o['remaining_v'] = vol-t_vol
+                split_o['remaining_v'] -= t_fit_vol
+                split_o['remaining_q'] -= t_fit_qty 
                 continue
             else:
-                # the remaining volume is smaller than the truck volume
-                t['orders'].append({split_o['order']:vol})
+                # the remaining split o volume is smaller than the truck volume
+                t['orders'].append({'order':sp_o_id, 'quantity':sp_qty, 'volume':sp_vol})
                 t['status']='part-full'
-                cur_o+=1                
+                vol+=sp_vol
+                cur_o+=1              
         while vol<t_vol and cur_o<len(orders):
             # take order
             o_id = orders.iloc[cur_o].name #['Order Number']
             o_vol = orders.iloc[cur_o]['Total Volume']
+            o_qty = orders.iloc[cur_o]['Balance Order Quantity']
+            o_t_v = orders.iloc[cur_o]['Volume per tyre']
             if vol+o_vol<=t_vol:
-                # add order to truck
-                t['orders'].append({o_id:o_vol})
+                # oder fits in the truck completely
+                t['orders'].append({'order':o_id, 'quantity':o_qty, 'volume':o_vol})
                 t['status']='part-full'
                 vol+=o_vol
                 cur_o+=1
             else:
                 t_rem_vol = t_vol-vol
-                if t_rem_vol==0:
-                    # the last order was exactly the same as the remaining volume
+                t_fit_qty = t_rem_vol//(o_t_v * volume_scale)
+                t_fit_vol = t_fit_qty*o_t_v * volume_scale
+                if t_rem_vol==0 or t_fit_qty==0:
+                    # the last order was exactly the same as the remaining volume or new order is too big (not even 1 tire fits)
                     t['status']='full'
                     split_o = None
                     break
@@ -73,30 +116,41 @@ def fill_trucks_customer(orders, trucks, wh_cap=25):
                 # Or first try to find a smaller order that can fit?
                 # Maybe this is better, analyse left volume first, and if there are no orders that can fit, split the order
                 # BuT this is all for one customer,so maybe it does not matter
-                o_rem_vol = o_vol-t_rem_vol
-                t['orders'].append({o_id:t_rem_vol})
+                t['orders'].append({'order':o_id, 'quantity':t_fit_qty, 'volume':t_fit_vol})
                 t['status']='full'
-                split_o = {'order':o_id, 'remaining_v':o_rem_vol}
+                split_o = {'order':o_id, 'remaining_v':o_vol-t_fit_vol, 'tire_v':o_t_v, 'remaining_q':o_qty-t_fit_qty}
                 break
         if t['status']=='full':
+            if date:
+                t['date'] = date
             booked_t.append(t)
             wh_cap-=1
+            booked_o_ids = [bo['order'] for bo in booked_o]
             for t_o in t['orders']:
                 # for each order in the truck
-                t_o_id = list(t_o.keys())[0]
-                t_o_v = t_o.values()[0]
-                booked_o.append({'order':t_o_id, 'truck':h_id,
-                                     'volume':})
+                t_o_id = t_o['order']
+                t_o_q = t_o['quantity']
+                t_o_v = t_o['volume']
+                if t_o_id not in booked_o_ids:
+                    booked_o.append({'order':t_o_id, 'truck':h_id,
+                        'volume':t_o_v, 'quantity':t_o_q})
+                else:
+                    for b_o in booked_o:
+                        if b_o['order']==t_o_id:
+                            b_o['volume']+=t_o_v
+                            b_o['quantity']+=t_o_q
         else:
             # this truck is not full, which means that there are no more orders
+            # Maybe here check how much space is left, maybe it's ok to leave it like this
             break
+
     # Return everything booked, everything left, and split order if existing, ad wh capacity left
     bk_o_ids = [bo['order'] for bo in booked_o]
-    rem_o = orders[~orders.index.isin(bk_o_ids)] # only full ord
+    rem_o = orders[~orders.index.isin(bk_o_ids)] # only full ord that are not booked
     if split_o:
         add_o = orders.loc[split_o['order']]
         add_o['Total Volume'] = split_o['remaining_v']
         add_o['Balance Order Quantity'] = split_o['remaining_q']
-        rem_o = rem_o.append(add_o)
+        rem_o = pd.concat([rem_o,add_o]) # add split order to the remaining orders
     #rem_t = trucks[~trucks['contractNumber'].isin([bt['truck'] for bt in booked_t])]
     return booked_t, booked_o, rem_o, wh_cap
